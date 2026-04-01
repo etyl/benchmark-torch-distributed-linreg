bfd883b188d537ac972f30eeaeaf9d4708ea4561
Hippolyte Verninas <etyl.sani@gmail.com>
2026-04-01 13:06:23 +0200

    config gpu

--- solvers/all_reduce.py
+++ solvers/all_reduce.py
@@ -38,7 +38,7 @@ class Solver(BaseSolver):
     parameters = {
         "local_batch_size": [32],
         "lr": [1e-3],
-        "slurm_gres": ["gpu:2"],
+        "gpus": [2]
     }
 
     requirements = ["pytorch:pytorch"]
